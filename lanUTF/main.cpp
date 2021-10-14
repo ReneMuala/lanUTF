1e0868deded207541678240c52466e4fc7aeb080
ReneMuala <renemuala@icloud.com>
2021-10-14 23:39:08 +0200

    Initial Commit

---
+++ lanUTF/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lanUTF
+//
+//  Created by René Descartes Domingos Muala on 14/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
